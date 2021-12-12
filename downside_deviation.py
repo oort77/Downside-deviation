db9afcf896b32f50839b15f6e4f29aa584b907e6
oort77 <73858914+oort77@users.noreply.github.com>
2021-12-12 05:50:30 +0300

    Update downside_deviation.py

--- downside_deviation.py
+++ downside_deviation.py
@@ -18,30 +18,34 @@ st.set_page_config(page_title='WHO COVID statistics',
 with open('./df.pickle', 'rb') as f:
     df = pickle.load(f)
 
-
+st.header('Downside deviation')
 target = st.sidebar.slider('Target', min_value=-15, max_value=40, value=1)
 
 col1, col2 = st.columns([3, 5])
 
 with col1:
-    st.header("Data")
+    st.subheader("Data: ")
+    st.markdown('<hr></hr>',unsafe_allow_html=True)
     df['Return-Target'] = df['Return'] - target
-    st.dataframe(df)
+    df['Diffs'] = target*(df['Return-Target']<0)
+    # df.set_index('Year', drop=True, inplace=True)
+    st.dataframe(df[['Year','Return','Return-Target']])
     # st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("Plot")
-
     downside_deviation = np.sqrt(
         ((df.loc[df['Return-Target'] < 0, 'Return-Target'])**2).sum()/df.shape[0])
     upside_deviation = np.sqrt(
         ((df.loc[df['Return-Target'] > 0, 'Return-Target'])**2).sum()/df.shape[0])
 
     st.write(
-        f'**Downside deviation** = {downside_deviation:.2f}, **upside deviation** = {upside_deviation:.2f}')
+        f'**Downside deviation** = {downside_deviation:.2f},  **upside deviation** = {upside_deviation:.2f}')
 
+
+with col2:
+    st.subheader("Plot")
+
+    
     fig, ax = plt.subplots(figsize=(11, 8))  # ,dpi=1000
-    df.plot.bar(x='Year', y='Return', color='b', ax=ax)
+    df.plot.bar(x='Year',y='Return', color='b', ax=ax)
     plt.annotate(f'Target: {target:.2f}', (-0.35, target+2))
     plt.annotate(f'Up dev: {upside_deviation:.2f}',
                  (-0.35, target+upside_deviation+2))
@@ -51,6 +55,9 @@ with col2:
     plt.axhline(y=0, xmin=-10, xmax=100,  color='b')
     plt.axhline(y=target - downside_deviation, xmin=-10, xmax=100, color='k')
     plt.axhline(y=target + upside_deviation, xmin=-10, xmax=100, color='g')
+    # for x_neg in df.loc[df['Return-Target'] < 0]:
+    #     plt.axvline(x = x_neg, ymin =df.loc[x_neg,'Return'],ymax=df.loc[x_neg, target]) 
+    df.plot.bar(x='Year', y='Diffs', color='purple', alpha=0.6, ax=ax)
     plt.legend()
     st.pyplot(fig)
 
